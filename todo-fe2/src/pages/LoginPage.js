3a7bd2ae6475d1a95cbd054d83cb2ab1063f2866
Hwiwon Kang <syaron9364@gmail.com>
2024-10-20 23:08:20 +0900

    fix: todo-fe2 form

--- todo-fe2/src/pages/LoginPage.js
+++ todo-fe2/src/pages/LoginPage.js
@@ -1,24 +1,21 @@
-import { Button, FormControl, styled } from "@mui/material";
-import React from "react";
+import {
+  Button,
+  FormControl,
+  Input,
+  InputLabel,
+  FormHelperText,
+  styled,
+} from "@mui/material";
+import React, { useState } from "react";
 import TodoContainer from "../components/common/TodoContainer";
+import api from "../utils/api";
+import { useNavigate } from "react-router-dom";
 
 const LoginContainer = styled("div")(() => ({
   padding: "2rem",
   width: "600px",
   backgroundColor: "#ffffff88",
   boxShadow: "#00000033 0px 0px 10px",
-}));
-
-const LoginInput = styled("input")(() => ({
-  border: "1px solid #e07368",
-  fontSize: "1.3rem",
-  outline: "none",
-  color: "#333",
-  fontFamily: "Dongle",
-  backgroundColor: "#ffffff99",
-  "&:focus": {
-    backgroundColor: "#e0736822",
-  },
 }));
 
 const LoginTitle = styled("h1")(() => ({
@@ -36,6 +33,30 @@ const LoginButton = styled(Button)(() => ({
 }));
 
 const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [joinedUser, setJoinedUser] = useState(null);
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await api.post("/user/login", { email, password });
+      if (res.status === 200) {
+        setJoinedUser(res.data.joinedUser);
+        sessionStorage.setItem("token", res.data.token);
+        api.defaults.headers["authorization"] = "Bearer " + res.data.token;
+        setError("");
+        navigate("/");
+      } else {
+        throw new Error("아이디 또는 비밀번호가 일치하지 않습니다");
+      }
+    } catch (err) {
+      setError(err.message);
+    }
+  };
+
   return (
     <TodoContainer
       sx={{
@@ -47,17 +68,38 @@ const LoginPage = () => {
     >
       <LoginContainer>
         <LoginTitle>Login</LoginTitle>
-        <form>
-          <FormControl fullWidth sx={{ mb: "10px" }}>
-            <label for={"login-email-input"}>Email Adress</label>
-            <LoginInput id={"login-email-input"} />
+        <form onSubmit={handleLogin}>
+          <FormControl fullWidth sx={{ mb: "10px" }} error={!!error}>
+            <InputLabel htmlFor="login-email-input">Email Address</InputLabel>
+            <Input
+              id="login-email-input"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              aria-describedby="email-helper-text"
+            />
+            <FormHelperText id="email-helper-text">
+              Enter your registered email address.
+            </FormHelperText>
           </FormControl>
-          <FormControl fullWidth sx={{ mb: "10px" }}>
-            <label for={"login-password-input"}>Password</label>
-            <LoginInput id={"login-password-input"} />
+
+          <FormControl fullWidth sx={{ mb: "10px" }} error={!!error}>
+            <InputLabel htmlFor="login-password-input">Password</InputLabel>
+            <Input
+              id="login-password-input"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              aria-describedby="password-helper-text"
+            />
+            <FormHelperText id="password-helper-text">
+              Enter your password.
+            </FormHelperText>
           </FormControl>
+
+          <LoginButton type="submit">Enter</LoginButton>
+          {error && <div>{error}</div>}
         </form>
-        <LoginButton>Enter</LoginButton>
       </LoginContainer>
     </TodoContainer>
   );
--- todo-fe2/src/pages/RegisterPage.js
+++ todo-fe2/src/pages/RegisterPage.js
@@ -1,24 +1,21 @@
-import { Button, FormControl, styled } from "@mui/material";
-import React from "react";
+import {
+  Button,
+  FormControl,
+  FormHelperText,
+  Input,
+  InputLabel,
+  styled,
+} from "@mui/material";
+import React, { useState } from "react";
 import TodoContainer from "../components/common/TodoContainer";
+import { useNavigate } from "react-router-dom";
+import api from "../utils/api";
 
 const RegisterContainer = styled("div")(() => ({
   padding: "2rem",
   width: "600px",
   backgroundColor: "#ffffff88",
   boxShadow: "#00000033 0px 0px 10px",
-}));
-
-const RegisterInput = styled("input")(() => ({
-  border: "1px solid #e07368",
-  fontSize: "1.3rem",
-  outline: "none",
-  color: "#333",
-  fontFamily: "Dongle",
-  backgroundColor: "#ffffff99",
-  "&:focus": {
-    backgroundColor: "#e0736822",
-  },
 }));
 
 const RegisterTitle = styled("h1")(() => ({
@@ -36,6 +33,30 @@ const RegisterButton = styled(Button)(() => ({
 }));
 
 const RegisterPage = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      if (password !== confirmPassword) {
+        throw new Error("패스워드가 일치하지 않습니다");
+      }
+      const res = await api.post("/user", { name, email, password });
+      if (res.status === 200) {
+        navigate("/login");
+      } else {
+        alert(error);
+      }
+    } catch (err) {
+      setError(err.message);
+    }
+  };
+
   return (
     <TodoContainer
       sx={{
@@ -47,27 +68,68 @@ const RegisterPage = () => {
     >
       <RegisterContainer>
         <RegisterTitle>Register</RegisterTitle>
-        <form>
+        <form onSubmit={handleSubmit}>
           <FormControl fullWidth sx={{ mb: "10px" }}>
-            <label for={"register-name-input"}>Name</label>
-            <RegisterInput id={"register-name-input"} />
+            <InputLabel htmlFor="register-name-input">Name</InputLabel>
+            <Input
+              id="register-name-input"
+              onChange={(e) => setName(e.target.value)}
+              aria-describedby="register-name-helper-text"
+              required
+            />
+            <FormHelperText id="register-name-helper-text">
+              Please enter your full name
+            </FormHelperText>
           </FormControl>
+
           <FormControl fullWidth sx={{ mb: "10px" }}>
-            <label for={"register-email-input"}>Email Adress</label>
-            <RegisterInput id={"register-email-input"} />
+            <InputLabel htmlFor="register-email-input">
+              Email Address
+            </InputLabel>
+            <Input
+              id="register-email-input"
+              onChange={(e) => setEmail(e.target.value)}
+              aria-describedby="register-email-helper-text"
+              required
+            />
+            <FormHelperText id="register-email-helper-text">
+              Enter a valid email address
+            </FormHelperText>
           </FormControl>
+
           <FormControl fullWidth sx={{ mb: "10px" }}>
-            <label for={"register-password-input"}>Password</label>
-            <RegisterInput id={"register-password-input"} />
+            <InputLabel htmlFor="register-password-input">Password</InputLabel>
+            <Input
+              type="password"
+              id="register-password-input"
+              onChange={(e) => setPassword(e.target.value)}
+              aria-describedby="register-password-helper-text"
+              required
+            />
+            <FormHelperText id="register-password-helper-text">
+              Choose a secure password
+            </FormHelperText>
           </FormControl>
+
           <FormControl fullWidth sx={{ mb: "10px" }}>
-            <label for={"register-confirm-password-input"}>
+            <InputLabel htmlFor="register-confirm-password-input">
               Confirm Password
-            </label>
-            <RegisterInput id={"register-confirm-password-input"} />
+            </InputLabel>
+            <Input
+              type="password"
+              id="register-confirm-password-input"
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              aria-describedby="register-confirm-password-helper-text"
+              required
+            />
+            <FormHelperText id="register-confirm-password-helper-text">
+              Re-enter your password
+            </FormHelperText>
           </FormControl>
+
+          <RegisterButton type="submit">Join Account</RegisterButton>
+          {error && <div>{error}</div>}
         </form>
-        <RegisterButton>Join Account</RegisterButton>
       </RegisterContainer>
     </TodoContainer>
   );
